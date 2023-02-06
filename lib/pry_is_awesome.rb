778c4dd5a83181ac7f0ab4e8190ce2dbc2ba7ace
Stacy Vana <98608384+ssvana83@users.noreply.github.com>
2023-02-06 16:00:29 -0500

    commit tree 8f52f7054f2866ca12117a1b5cc3266d5c20beca

--- lib/pry_is_awesome.rb
+++ lib/pry_is_awesome.rb
@@ -1,5 +1,7 @@
 require 'pry'
+# this is a method tht loads the pry library in the file when app runs(similar to import in React)
 
+# now we define a method
 def prying_into_the_method
 	inside_the_method = "We're inside the method"
 	puts inside_the_method
@@ -9,4 +11,6 @@ def prying_into_the_method
 	puts this_variable_hasnt_been_interpreted_yet
 end
 
+
+# this is where we call the method
 prying_into_the_method
